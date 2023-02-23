cc22892caa0b1cd930e9706c8637b71c14a48d47
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-02-23 15:31:42 +0800

    types-of-repairs

--- scrypt.js
+++ scrypt.js
@@ -25,10 +25,10 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 16,
 });
 
-// function readMore(selector, readMoreItem, hideReadMore) {
+// function readMoreBrends(selector, readMoreItem, hideReadMoreBrends) {
 //   const elementsView = document.querySelectorAll(selector);
 //   const elementRead = document.querySelector(readMoreItem);
-//   const elementHideReadMore = document.querySelector(hideReadMore);
+//   const elementHideReadMore = document.querySelector(hideReadMoreBrends);
 
 //   for (let i = 0; i < elementsView.length; i++) {
 //     elementsView[i].style.display = "block";
@@ -38,10 +38,10 @@ const swiper = new Swiper(".swiper", {
 //   elementHideReadMore.style.display = "block";
 // }
 
-// function hideReadMore(selector, readMoreItem, hideReadMore) {
+// function hideReadMoreBrends(selector, readMoreItem, hideReadMoreBrends) {
 //   const elementsView = document.querySelectorAll(selector);
 //   const elementRead = document.querySelector(readMoreItem);
-//   const elementHideReadMore = document.querySelector(hideReadMore);
+//   const elementHideReadMore = document.querySelector(hideReadMoreBrends);
 
 //   for (let i = 0; i < elementsView.length; i++) {
 //     elementsView[i].style.display = "none";
@@ -50,12 +50,11 @@ const swiper = new Swiper(".swiper", {
 //   elementRead.style.display = "none";
 //   elementHideReadMore.style.display = "block";
 // }
-console.log(hi);
 
-function readMore(selector, readMoreItem, hideReadMore) {
+function readMoreBrends(selector, readMoreItem, hideReadMoreBrends) {
   const elementsView = document.querySelectorAll(selector);
   const elementRead = document.querySelector(readMoreItem);
-  const elementHideReadMore = document.querySelector(hideReadMore);
+  const elementHideReadMore = document.querySelector(hideReadMoreBrends);
 
   for (let i = 0; i < elementsView.length; i++) {
     elementsView[i].classList.add("display-slider-view-read-more");
@@ -65,10 +64,10 @@ function readMore(selector, readMoreItem, hideReadMore) {
   elementHideReadMore.style.display = "block";
 }
 
-function hideReadMore(selector, readMoreItem, hideReadMore) {
+function hideReadMoreBrends(selector, readMoreItem, hideReadMoreBrends) {
   const elementsView = document.querySelectorAll(selector);
   const elementRead = document.querySelector(readMoreItem);
-  const elementHideReadMore = document.querySelector(hideReadMore);
+  const elementHideReadMore = document.querySelector(hideReadMoreBrends);
 
   for (let i = 0; i < elementsView.length; i++) {
     elementsView[i].classList.remove("display-slider-view-read-more");
@@ -76,4 +75,44 @@ function hideReadMore(selector, readMoreItem, hideReadMore) {
 
   elementRead.style.display = "none";
   elementHideReadMore.style.display = "block";
+}
+
+// Эта функции скрытия типов ремонта
+
+function readMoreTypes() {
+  const typesRepairs = document.querySelector(".types-of-repairs");
+  console.log(typesRepairs);
+
+  // Кнопка которая изначально видно и кнопка которую не видно
+  const buttonVis = document.querySelector(".button-read-hid");
+  const buttonHid = document.querySelector(".button-read-vis");
+
+  console.log(buttonHid);
+
+  // Меняю свойства родительского контейнера на wrap для переноса строки чтобы все элементы стало видно
+
+  typesRepairs.classList.add("display-wrap");
+
+  // Одну кнопку скрываю другую показываю
+  buttonHid.style.display = "none";
+  buttonVis.style.display = "block";
+}
+
+function hideReadMoreTypes() {
+  const typesRepairs = document.querySelector(".types-of-repairs");
+  console.log(typesRepairs);
+
+  // Кнопка которая изначально видно и кнопка которую не видно
+  const buttonVis = document.querySelector(".button-read-hid");
+  const buttonHid = document.querySelector(".button-read-vis");
+
+  console.log(buttonHid);
+
+  // Меняю свойства родительского контейнера на wrap для переноса строки чтобы все элементы стало видно
+
+  typesRepairs.classList.remove("display-wrap");
+
+  // Одну кнопку скрываю другую показываю
+  buttonVis.style.display = "none";
+  buttonHid.style.display = "block";
 }
