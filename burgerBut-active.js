9c4976022fdc9297683335b489cdcb7fcc35c290
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-03-12 13:21:28 +0800

    burger-ok

--- burgerBut-active.js
+++ burgerBut-active.js
@@ -1,16 +1,15 @@
-function activeSubtitleBut(buttonSub) {
-  console.log("work");
+function activeBurgerBut(buttonBur) {
   // Получаем коллекцию всех кнопок
-  let buttons = document.getElementsByClassName("subtitle__box");
+  let buttons = document.getElementsByClassName("nav-burger-item");
 
   //У всех кнопок удаляем класс активный
   for (let i = 0; i < buttons.length; i++) {
     const element = buttons[i];
 
-    element.classList.remove("subtitle--active");
+    element.classList.remove("nav-burger-item--active");
   }
 
   // Добавляем класс кнопке через её ID который был передан внутри кнопки как параметр функции
-  let button = document.getElementById(buttonSub);
-  button.classList.add("subtitle--active");
+  let button = document.getElementById(buttonBur);
+  button.classList.add("nav-burger-item--active");
 }
